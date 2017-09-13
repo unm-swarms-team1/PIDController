8862ccce8d8372de7d43cd54d246b917e1171d08
Will Vining <wfvining@gmail.com>
2017-09-13 10:19:40 -0600

    Corrected bug in computation of the integral.
    Forgot to multiply by delta-t when computing the integral.

--- PIDController.m
+++ PIDController.m
@@ -7,16 +7,25 @@ k_d = 0.00002;
 k_i = 0.0000048;
 
 persistent error_integral
+persistent last_time
 
 error = [joint1_angle_setpoint - joint1_measured_angle; joint2_angle_setpoint - joint2_measured_angle];
 
+if isempty(last_time) 
+    last_time = 0.0;
+end
+dt = current_time - last_time;
+last_time = current_time;       
+
 if isempty(error_integral)
-    error_integral = error;
+    error_integral = error*dt;
 else
-    error_integral = error_integral + error;
+    error_integral = error_integral + (error*dt);
 end
 
-torque = k_p * error + k_d * [error1_dot; error2_dot] + k_i * error_integral;
+error_dot = [error1_dot; error2_dot];
+
+torque = k_p * error + k_d * error_dot + k_i * error_integral;
 
 control_forces = torque;
 
