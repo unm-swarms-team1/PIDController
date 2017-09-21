29bcc77786914d7603f431c4eb34579dc1876bc6
Will Vining <wfvining@gmail.com>
2017-09-21 12:18:54 -0600

    Updated the PID to use a custom derivative term
    This update also requires changing the derivative gain to be a positive value, since we are computing the actual derivative rather than relying on the negative derivative passed in to the function.

--- PIDController.m
+++ PIDController.m
@@ -3,11 +3,13 @@
 function control_forces = PIDController( current_time, joint1_angle_setpoint, joint2_angle_setpoint, joint1_measured_angle, joint2_measured_angle, error1_dot, error2_dot )
 
 k_p = 4000;
-k_d = -200;
+k_d = 200;
 k_i = 4000;
 
 persistent error_integral
 persistent last_time
+persistent last_error
+persistent error_dot
 
 error = [joint1_angle_setpoint - joint1_measured_angle; joint2_angle_setpoint - joint2_measured_angle];
 
@@ -23,7 +25,19 @@ else
     error_integral = error_integral + (error*dt);
 end
 
-error_dot = [error1_dot; error2_dot];
+if isempty(last_error)
+    last_error = [0.0;0.0];
+end
+
+if isempty(error_dot)
+    error_dot = [0.0;0.0];
+end
+
+if  dt ~= 0.0
+    error_dot = (error - last_error)./dt;
+end
+
+last_error = error;
 
 torque = k_p * error + k_d * error_dot + k_i * error_integral;
 
